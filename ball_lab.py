7dc234cfef48665edcbb0a2e9e36085252f4717e
Student <student@client2110.(none)>
2019-12-13 16:13:45 +0300

    first step of lab ball

---
+++ ball_lab.py
@@ -0,0 +1,38 @@
+from tkinter import *
+from random import randrange as rnd, choice
+import time
+root = Tk()
+root.geometry('800x600')
+
+canv = Canvas(root,bg='white')
+canv.pack(fill=BOTH,expand=1)
+
+colors = ['red','orange','yellow','green','blue']
+def new_ball():
+    canv.delete(ALL)
+    x = rnd(100,700)
+    y = rnd(100,500)
+    r = rnd(30,50)
+    canv.create_oval(x-r,y-r,x+r,y+r,fill = choice(colors), width=0)
+    root.after(1000,new_ball)
+
+
+def click(event):
+    print('click')
+
+new_ball()
+canv.bind('<Button-1>', click)
+
+def new_ball():
+    global x,y,r
+    canv.delete(ALL)
+    x = rnd(100,700)
+    y = rnd(100,500)
+    r = rnd(30,50)
+    canv.create_oval(x-r,y-r,x+r,y+r,fill = choice(colors), width=0)
+    root.after(1000,new_ball)
+
+
+def click(event):
+    print(x,y,r)
+mainloop()
